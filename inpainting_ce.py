bb94f32ffcb63a595f4ffa3f64c110a84737146e
ombretta <ombretta.st@gmail.com>
2023-05-10 13:58:07 +0200

    restructured inpainting_ce.py

--- inpainting_ce.py
+++ inpainting_ce.py
@@ -40,130 +40,172 @@ def load_image_and_mask(img_path, mask_path, imsize, dim_div_by):
     return img_np, img_mask_np
 
 
-def main(net_type='skip_depth6', img_name='vase'):
+class InpaintingConfigs(object):
 
-    PLOT = True
-    imsize = -1
-    dim_div_by = 64
+    def __init__(self, net_type, img_name):
+        (net, input_depth, input, img_np, img_mask_np, param_noise,
+         reg_noise_std, figsize, out_path, OPT_OVER, OPTIMIZER, LR,
+         num_iter) = self.get_configs(net_type, img_name)
+        self.net = net
+        self.input_depth = input_depth
+        self.input = input
+        self.img_np = img_np
+        self.img_mask_np = img_mask_np
+        self.param_noise = param_noise
+        self.reg_noise_std = reg_noise_std
+        self.figsize = figsize
+        self.out_path = out_path
+        self.OPT_OVER = OPT_OVER
+        self.OPTIMIZER = OPTIMIZER
+        self.LR = LR
+        self.num_iter = num_iter
 
-    # Choose net type
-    NET_TYPE = net_type  # one of skip_depth6|skip_depth4|skip_depth2|UNET|ResNet
+    
+    def get_configs(self, net_type, img_name):
+        
+        # Input image params
+        imsize = -1
+        dim_div_by = 64
+        img_path = 'data/inpainting/' + img_name + '.png'
+        mask_path = 'data/inpainting/' + img_name + '_mask.png'
 
-    # Choose figure
-    IMG_NAME = img_name #'vase' #'library' #'kate'
-    img_path = 'data/inpainting/'+IMG_NAME+'.png'
-    mask_path = 'data/inpainting/'+IMG_NAME+'_mask.png'
+        # Load image and mask
+        img_np, img_mask_np = load_image_and_mask(img_path, mask_path, imsize, dim_div_by)
 
-    # Define output path
-    out_path = 'results/inpainting/'+NET_TYPE+'/'+IMG_NAME+'/'
-    if not os.path.exists('results/inpainting/'+NET_TYPE):
-        os.mkdir('results/inpainting/'+NET_TYPE)
-    if not os.path.exists(out_path):
-        os.mkdir(out_path)
+        # Output paths
+        out_path = 'results/inpainting/' + net_type + '/' + img_name + '/'
+        if not os.path.exists('results/inpainting/' + net_type):
+            os.mkdir('results/inpainting/' + net_type)
+        if not os.path.exists(out_path):
+            os.mkdir(out_path)
 
-    # Load image and mask
-    img_np, img_mask_np = load_image_and_mask(img_path, mask_path, imsize, dim_div_by)
+        # Network/optimizer setup
+        pad = 'zero'  # 'reflection'
+        OPT_OVER = 'net'
+        OPTIMIZER = 'adam'
 
-    # Setup
-    pad = 'zero' #'reflection'
-    OPT_OVER = 'net'
-    OPTIMIZER = 'adam'
-
-    if 'vase.png' in img_path:
-        INPUT = 'meshgrid'
-        input_depth = 2
-        LR = 0.01
-        num_iter = 5001
-        param_noise = False
-        show_every = 500
-        figsize = 5
-        reg_noise_std = 0.03
-
-        net = ce_skip(in_rotations=1, out_rotations=3,
-                      num_input_channels=input_depth,
-                      num_output_channels=img_np.shape[0],
-                      num_channels_down=[128] * 5,
-                      num_channels_up=[128] * 5,
-                      num_channels_skip=[0] * 5,
-                      upsample_mode='nearest', filter_skip_size=1, filter_size_up=3, filter_size_down=3,
-                      need_sigmoid=True, need_bias=True, pad=pad, act_fun='LeakyReLU').type(dtype)
-
-        print(net)
-
-    elif ('kate.png' in img_path) or ('peppers.png' in img_path):
-        # Same params and net as in super-resolution and denoising
-        INPUT = 'noise'
-        input_depth = 32
-        LR = 0.01
-        num_iter = 6001
-        param_noise = False
-        show_every = 500
-        figsize = 5
-        reg_noise_std = 0.03
-
-        net = ce_skip(in_rotations=1, out_rotations=3,
-                      num_input_channels=input_depth,
-                      num_output_channels=img_np.shape[0],
-                      num_channels_down=[128] * 5,
-                      num_channels_up=[128] * 5,
-                      num_channels_skip=[128] * 5,
-                      filter_size_up=3, filter_size_down=3,
-                      upsample_mode='nearest', filter_skip_size=1,
-                      need_sigmoid=True, need_bias=True, pad=pad,
-                      act_fun='LeakyReLU').type(dtype)
-
-    elif 'library.png' in img_path:
-
-        INPUT = 'noise'
-        input_depth = 1
-
-        num_iter = 3001
-        show_every = 500
-        figsize = 8
-        reg_noise_std = 0.00
-        param_noise = True
-
-        if 'skip' in NET_TYPE:
-
-            depth = int(NET_TYPE[-1])
-            net = ce_skip(in_rotations=1, out_rotations=3,
-                       num_input_channels=input_depth,
-                       num_output_channels=img_np.shape[0],
-                       num_channels_down=[16, 32, 64, 128, 128, 128][:depth],
-                       num_channels_up=[16, 32, 64, 128, 128, 128][:depth],
-                       num_channels_skip=[0, 0, 0, 0, 0, 0][:depth],
-                       filter_size_up=3, filter_size_down=5, filter_skip_size=1,
-                       upsample_mode='nearest',  # downsample_mode='avg',
-                       need1x1_up=False,
-                       need_sigmoid=True, need_bias=True, pad=pad, act_fun='LeakyReLU').type(dtype)
-
+        # Optimization and model
+        if 'vase.png' in img_path:
+            input = 'meshgrid'
+            input_depth = 2
             LR = 0.01
-
-        elif NET_TYPE == 'UNET':
-
-            net = UNet(num_input_channels=input_depth, num_output_channels=3,
-                       feature_scale=8, more_layers=1,
-                       concat_x=False, upsample_mode='deconv',
-                       pad=pad, norm_layer=torch.nn.InstanceNorm2d, need_sigmoid=True, need_bias=True)
-
-            LR = 0.001
+            num_iter = 5001
             param_noise = False
+            figsize = 5
+            reg_noise_std = 0.03
 
-        elif NET_TYPE == 'ResNet':
+            if "ce_skip" in net_type:
+                net = ce_skip(in_rotations=1, out_rotations=3,
+                              num_input_channels=input_depth,
+                              num_output_channels=img_np.shape[0],
+                              num_channels_down=[128] * 5,
+                              num_channels_up=[128] * 5,
+                              num_channels_skip=[0] * 5,
+                              upsample_mode='nearest', filter_skip_size=1, filter_size_up=3, filter_size_down=3,
+                              need_sigmoid=True, need_bias=True, pad=pad, act_fun='LeakyReLU').type(dtype)
+            else:
+                net = skip(input_depth, img_np.shape[0],
+                           num_channels_down=[128] * 5,
+                           num_channels_up=[128] * 5,
+                           num_channels_skip=[0] * 5,
+                           upsample_mode='nearest', filter_skip_size=1, filter_size_up=3, filter_size_down=3,
+                           need_sigmoid=True, need_bias=True, pad=pad, act_fun='LeakyReLU').type(dtype)
 
-            net = ResNet(input_depth, img_np.shape[0], 8, 32, need_sigmoid=True, act_fun='LeakyReLU')
-
-            LR = 0.001
+        elif ('kate.png' in self.img_path) or ('peppers.png' in self.img_path):
+            # Same params and net as in super-resolution and denoising
+            input = 'noise'
+            input_depth = 32
+            LR = 0.01
+            num_iter = 6001
             param_noise = False
+            figsize = 5
+            reg_noise_std = 0.03
 
+            if "ce_skip" in net_type:
+                net = ce_skip(in_rotations=1, out_rotations=3,
+                          num_input_channels=input_depth,
+                          num_output_channels=img_np.shape[0],
+                          num_channels_down=[128] * 5,
+                          num_channels_up=[128] * 5,
+                          num_channels_skip=[128] * 5,
+                          filter_size_up=3, filter_size_down=3,
+                          upsample_mode='nearest', filter_skip_size=1,
+                          need_sigmoid=True, need_bias=True, pad=pad,
+                          act_fun='LeakyReLU').type(dtype)
+            else:
+                net = skip(input_depth, img_np.shape[0],
+                           num_channels_down=[128] * 5,
+                           num_channels_up=[128] * 5,
+                           num_channels_skip=[128] * 5,
+                           filter_size_up=3, filter_size_down=3,
+                           upsample_mode='nearest', filter_skip_size=1,
+                           need_sigmoid=True, need_bias=True, pad=pad, act_fun='LeakyReLU').type(dtype)
+
+        elif 'library.png' in self.img_path:
+
+            input = 'noise'
+            input_depth = 1
+            num_iter = 3001
+            figsize = 8
+            reg_noise_std = 0.00
+            param_noise = True
+
+            if 'skip' in net_type:
+
+                depth = int(net_type[-1])
+                if "ce_skip" in net_type:
+                    net = ce_skip(in_rotations=1, out_rotations=3,
+                              num_input_channels=input_depth,
+                              num_output_channels=img_np.shape[0],
+                              num_channels_down=[16, 32, 64, 128, 128, 128][:depth],
+                              num_channels_up=[16, 32, 64, 128, 128, 128][:depth],
+                              num_channels_skip=[0, 0, 0, 0, 0, 0][:depth],
+                              filter_size_up=3, filter_size_down=5, filter_skip_size=1,
+                              upsample_mode='nearest',  # downsample_mode='avg',
+                              need1x1_up=False,
+                              need_sigmoid=True, need_bias=True, pad=pad, act_fun='LeakyReLU').type(dtype)
+                else:
+                    net = skip(input_depth, img_np.shape[0],
+                               num_channels_down=[16, 32, 64, 128, 128, 128][:depth],
+                               num_channels_up=[16, 32, 64, 128, 128, 128][:depth],
+                               num_channels_skip=[0, 0, 0, 0, 0, 0][:depth],
+                               filter_size_up=3, filter_size_down=5, filter_skip_size=1,
+                               upsample_mode='nearest',  # downsample_mode='avg',
+                               need1x1_up=False,
+                               need_sigmoid=True, need_bias=True, pad=pad, act_fun='LeakyReLU').type(dtype)
+
+                LR = 0.01
+
+            elif net_type == 'UNET':
+                net = UNet(num_input_channels=input_depth, num_output_channels=3,
+                           feature_scale=8, more_layers=1,
+                           concat_x=False, upsample_mode='deconv',
+                           pad=pad, norm_layer=torch.nn.InstanceNorm2d, need_sigmoid=True, need_bias=True)
+                LR = 0.001
+                param_noise = False
+
+            elif net_type == 'ResNet':
+                net = ResNet(input_depth, img_np.shape[0], 8, 32, need_sigmoid=True, act_fun='LeakyReLU')
+                LR = 0.001
+                param_noise = False
+
+            else:
+                assert False
         else:
             assert False
-    else:
-        assert False
+        
+        return (net, input_depth, input, img_np, img_mask_np,
+                param_noise, reg_noise_std, figsize, out_path,
+                OPT_OVER, OPTIMIZER, LR, num_iter)
 
-    net = net.type(dtype)
-    net_input = get_noise(input_depth, INPUT, img_np.shape[1:]).type(dtype)
-    # print("Net input 0", net_input.shape)
+
+def main(net_type='skip_depth6', img_name='vase', PLOT = True, show_every=500):
+
+    confs = InpaintingConfigs(net_type, img_name)
+    print(confs.net)
+
+    net = confs.net.type(dtype)
+    net_input = get_noise(confs.input_depth, confs.input, confs.img_np.shape[1:]).type(dtype)
 
     # Compute number of parameters
     s = sum(np.prod(list(p.size())) for p in net.parameters())
@@ -172,28 +214,23 @@ def main(net_type='skip_depth6', img_name='vase'):
     # Loss
     mse = torch.nn.MSELoss().type(dtype)
 
-    img_var = np_to_torch(img_np).type(dtype)
-    mask_var = np_to_torch(img_mask_np).type(dtype)
+    img_var = np_to_torch(confs.img_np).type(dtype)
+    mask_var = np_to_torch(confs.img_mask_np).type(dtype)
 
     # Main loop
 
     def closure():
         global i
 
-        if param_noise:
+        if confs.param_noise:
             for n in [x for x in net.parameters() if len(x.size()) == 4]:
                 n = n + n.detach().clone().normal_() * n.std() / 50
 
-        # print("Net input saved", net_input_saved.shape)
-
         net_input = net_input_saved
-        if reg_noise_std > 0:
-            net_input = net_input_saved + (noise.normal_() * reg_noise_std)
+        if confs.reg_noise_std > 0:
+            net_input = net_input_saved + (noise.normal_() * confs.reg_noise_std)
 
-        # print("Net input", net_input.shape)
         out = net(net_input)
-
-        # print("Net output", out.shape)
 
         total_loss = mse(out * mask_var, img_var * mask_var)
         total_loss.backward()
@@ -201,10 +238,10 @@ def main(net_type='skip_depth6', img_name='vase'):
         print('Iteration %05d    Loss %f' % (i, total_loss.item()), '\r', end='')
         if PLOT and i % show_every == 0:
             out_np = torch_to_np(out)
-            plot_image_grid([np.clip(out_np, 0, 1)], factor=figsize, nrow=1)
+            plot_image_grid([np.clip(out_np, 0, 1)], factor=confs.figsize, nrow=1)
             out_pil = np_to_pil(out_np)
-            print(os.path.join(out_path, "iter_"+str(i)+".png"))
-            out_pil.save(os.path.join(out_path, "iter_"+str(i)+".png"))
+            print(os.path.join(confs.out_path, "iter_"+str(i)+".png"))
+            out_pil.save(os.path.join(confs.out_path, "iter_"+str(i)+".png"))
         i += 1
 
         return total_loss
@@ -212,8 +249,8 @@ def main(net_type='skip_depth6', img_name='vase'):
     net_input_saved = net_input.detach().clone()
     noise = net_input.detach().clone()
 
-    p = get_params(OPT_OVER, net, net_input)
-    optimize(OPTIMIZER, p, closure, LR, num_iter)
+    p = get_params(confs.OPT_OVER, net, net_input)
+    optimize(confs.OPTIMIZER, p, closure, confs.LR, confs.num_iter)
     # %%
     out_np = torch_to_np(net(net_input))
     plot_image_grid([out_np], factor=5);
